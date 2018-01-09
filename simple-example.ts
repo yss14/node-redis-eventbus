0b7d8735b4777102f592ec626dc3a0d98902ea85
Yss <yannickstach@googlemail.com>
2018-01-09 17:31:11 +0100

    Extended simple example

--- simple-example.ts
+++ simple-example.ts
@@ -4,7 +4,15 @@ import { setTimeout } from 'timers';
 const eventBus = EventBus.create('myEventBus');
 
 (async () => {
-	await eventBus.on<string>('msg', (payload) => console.log(`Received message: ${payload}`));
+	await eventBus.on<string>('msg', (payload) => {
+		console.log(`Received message: ${payload}`);
+
+		eventBus.destory();
+
+		process.exit();
+	});
+
+	console.log('Listening to msg event');
 
 	setTimeout(() => {
 		eventBus.emit<string>('msg', 'Hello');
