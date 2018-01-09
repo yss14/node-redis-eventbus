f5143ea01b403e7dbeaaafe8b9b0eea9764f6b54
Yss <yannickstach@googlemail.com>
2018-01-09 23:08:27 +0100

    Added init(); Made create() async; Implemented ping() method

--- src/EventBus.ts
+++ src/EventBus.ts
@@ -1,4 +1,6 @@
 import * as Redis from 'redis';
+import { resolve } from 'path';
+import { clearTimeout } from 'timers';
 
 export class EventBus {
 	//Static variables
@@ -22,9 +24,26 @@ export class EventBus {
 		}
 	}
 
+	private async init(): Promise<void> {
+		//Listen for ping event
+		await this._on<void>('ping', () => {
+			//Emit pong event
+			this._emit('pong', '', true);
+		}, true);
+	}
+
 	//Register event listener
 	public on<T>(event: string, callback: (payload: T) => void): Promise<void> {
+		return this._on<T>(event, callback, false);
+	}
+
+	private _on<T>(event: string, callback: (payload: T) => void, internalCall: boolean = true): Promise<void> {
 		return new Promise<void>((resolve, reject) => {
+			//Check if event is reserved event name
+			if (!internalCall && this.isReservedEventName(event)) {
+				reject(new Error(`Reserved event name ${event} cannot be registered`));
+			}
+
 			//Listen for the subscribe event
 			this._sub.on('subscribe', (channel: string, count: number) => {
 				let _channel = this.removePrefixFromChannelName(channel);
@@ -50,7 +69,45 @@ export class EventBus {
 
 	//Emit a new event to the event bus
 	public emit<T>(event: string, payload: T): void {
+		return this._emit(event, payload, false);
+	}
+
+	private _emit<T>(event: string, payload: T, internalCall: boolean = true): void {
+		//Check if event is reserved event name
+		if (!internalCall && this.isReservedEventName(event)) {
+			throw new Error(`Reserved event name ${event} cannot be registered`);
+		}
+
 		this._pub.publish(this.getPrefixedChannelName(event), typeof payload === 'string' ? payload : JSON.stringify(payload));
+	}
+
+	/*
+		Send out a ping to all connected instances
+		Waits for <timeout> seconds and returnes true, if at least <minResponseCount> clients responded
+	*/
+	public ping(timeout: number = 3000, minResponseCount: number = 1): Promise<boolean> {
+		return new Promise<boolean>((resolve) => {
+			let responseCount = 0;
+
+			const timeoutRef = setTimeout(() => {
+				//+1 because our instance itself will respond
+				resolve(responseCount >= minResponseCount + 1);
+			}, timeout);
+
+			this._on<void>('pong', () => {
+				responseCount++;
+
+				//+1 because our instance itself will respond
+				if (responseCount >= minResponseCount + 1) {
+					//Cleat timeout
+					clearTimeout(timeoutRef);
+
+					resolve(true);
+				}
+			}, true);
+
+			this._emit('ping', '', true);
+		});
 	}
 
 	//Destroy event bus instance
@@ -87,8 +144,12 @@ export class EventBus {
 		return `${this._prefix.length > 0 ? `${this._prefix}:` : ''}${channel}`;
 	}
 
+	private isReservedEventName(event: string): boolean {
+		return event === 'ping' || event === 'pong';
+	}
+
 	//Factory method
-	public static create(name: string, clientOpts?: Redis.ClientOpts): EventBus {
+	public async static create(name: string, clientOpts?: Redis.ClientOpts): Promise<EventBus> {
 		//Lazy init event bus instances map
 		if (EventBus._eventBusInstances === undefined || EventBus._eventBusInstances === null) {
 			EventBus._eventBusInstances = new Map<string, EventBus>();
@@ -114,6 +175,9 @@ export class EventBus {
 		} else {
 			//Create new instance
 			instance = new EventBus(name, clientOpts);
+
+			//Init instance
+			await instance.init();
 
 			//Store to map
 			EventBus._eventBusInstances.set(name, instance);
