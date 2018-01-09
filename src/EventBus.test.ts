0b6b9d62fbdfdee230045a0949a0a7ded4eff7ea
Yss <yannickstach@googlemail.com>
2018-01-09 17:17:33 +0100

    Added test cases for EventBus

---
+++ src/EventBus.test.ts
@@ -0,0 +1,166 @@
+import { EventBus } from './EventBus';
+
+test('create new instance', async () => {
+	const eventBusInstance = EventBus.create('myEventBus_test1');
+
+	eventBusInstance.destory();
+});
+
+test('send single message with one event', async () => {
+	const eventBusInstance = EventBus.create('myEventBus_test2');
+
+	const MESSGAE_TO_SEND = 'Hello';
+	const EVENT = 'msg';
+
+	const result = await new Promise<string>(async (resolve, reject) => {
+		await eventBusInstance.on<string>(EVENT, (payload) => {
+			resolve(payload);
+		});
+
+		eventBusInstance.emit<string>(EVENT, MESSGAE_TO_SEND);
+	});
+
+	expect(result).toBe(MESSGAE_TO_SEND);
+
+	eventBusInstance.destory();
+});
+
+test('send multiple messages with two event', async () => {
+	const eventBusInstance = EventBus.create('myEventBus_test3');
+
+	const MESSGAE_TO_SEND1 = 'Hello';
+	const MESSGAE_TO_SEND2 = 'World';
+	const MESSGAE_TO_SEND3 = 'How';
+	const MESSGAE_TO_SEND4 = 'are';
+	const MESSGAE_TO_SEND5 = 'you?';
+	const EVENT1 = 'hello';
+	const EVENT2 = 'ask';
+
+	const result = await Promise.all([
+		new Promise<string[]>(async (resolve, reject) => {
+			let receivedMessages: string[] = [];
+
+			await eventBusInstance.on<string>(EVENT1, (payload) => {
+				receivedMessages.push(payload);
+
+				if (receivedMessages.length === 2) {
+					resolve(receivedMessages);
+				}
+			});
+
+			eventBusInstance.emit<string>(EVENT1, MESSGAE_TO_SEND1);
+			eventBusInstance.emit<string>(EVENT1, MESSGAE_TO_SEND2);
+		}),
+		new Promise<string[]>(async (resolve, reject) => {
+			let receivedMessages: string[] = [];
+
+			await eventBusInstance.on<string>(EVENT2, (payload) => {
+				receivedMessages.push(payload);
+
+				if (receivedMessages.length === 3) {
+					resolve(receivedMessages);
+				}
+			});
+
+			eventBusInstance.emit<string>(EVENT2, MESSGAE_TO_SEND3);
+			eventBusInstance.emit<string>(EVENT2, MESSGAE_TO_SEND4);
+			eventBusInstance.emit<string>(EVENT2, MESSGAE_TO_SEND5);
+		})
+	]);
+
+	expect(result.length).toBe(2);
+	expect(result[0].indexOf(MESSGAE_TO_SEND1) > -1).toBe(true);
+	expect(result[0].indexOf(MESSGAE_TO_SEND2) > -1).toBe(true);
+	expect(result[1].indexOf(MESSGAE_TO_SEND3) > -1).toBe(true);
+	expect(result[1].indexOf(MESSGAE_TO_SEND4) > -1).toBe(true);
+	expect(result[1].indexOf(MESSGAE_TO_SEND5) > -1).toBe(true);
+
+	eventBusInstance.destory();
+});
+
+test('send multiple messages with two event on two buses', async () => {
+	const eventBusInstanceA = EventBus.create('myEventBus1_test4');
+	const eventBusInstanceB = EventBus.create('myEventBus2_test4');
+
+	const MESSGAE_TO_SEND1 = 'Hello';
+	const MESSGAE_TO_SEND2 = 'World';
+	const MESSGAE_TO_SEND3 = 'How';
+	const MESSGAE_TO_SEND4 = 'are';
+	const MESSGAE_TO_SEND5 = 'you?';
+	const EVENT1 = 'hello';
+	const EVENT2 = 'ask';
+
+	const result = await Promise.all([
+		new Promise<string[][]>(async (resolve, reject) => {
+			let receivedMessagesInstanceA: string[] = [];
+			let receivedMessagesInstanceB: string[] = [];
+
+			await eventBusInstanceA.on<string>(EVENT1, (payload) => {
+				receivedMessagesInstanceA.push(payload);
+
+				if (receivedMessagesInstanceA.length === 2 && receivedMessagesInstanceB.length === 2) {
+					resolve([receivedMessagesInstanceA, receivedMessagesInstanceB]);
+				}
+			});
+
+			await eventBusInstanceB.on<string>(EVENT1, (payload) => {
+				receivedMessagesInstanceB.push(payload);
+
+				if (receivedMessagesInstanceA.length === 2 && receivedMessagesInstanceB.length === 2) {
+					resolve([receivedMessagesInstanceA, receivedMessagesInstanceB]);
+				}
+			});
+
+			eventBusInstanceA.emit<string>(EVENT1, MESSGAE_TO_SEND1);
+			eventBusInstanceB.emit<string>(EVENT1, MESSGAE_TO_SEND2);
+		}),
+		new Promise<string[][]>(async (resolve, reject) => {
+			let receivedMessagesInstanceA: string[] = [];
+			let receivedMessagesInstanceB: string[] = [];
+
+			await eventBusInstanceA.on<string>(EVENT2, (payload) => {
+				receivedMessagesInstanceA.push(payload);
+
+				if (receivedMessagesInstanceA.length === 3 && receivedMessagesInstanceB.length === 3) {
+					resolve([receivedMessagesInstanceA, receivedMessagesInstanceB]);
+				}
+			});
+
+			await eventBusInstanceB.on<string>(EVENT2, (payload) => {
+				receivedMessagesInstanceB.push(payload);
+
+				if (receivedMessagesInstanceA.length === 3 && receivedMessagesInstanceB.length === 3) {
+					resolve([receivedMessagesInstanceA, receivedMessagesInstanceB]);
+				}
+			});
+
+			eventBusInstanceA.emit<string>(EVENT2, MESSGAE_TO_SEND3);
+			eventBusInstanceB.emit<string>(EVENT2, MESSGAE_TO_SEND4);
+			eventBusInstanceA.emit<string>(EVENT2, MESSGAE_TO_SEND5);
+		})
+	]);
+
+	//array [<event>][<instance>][<messages>]
+	expect(result.length).toBe(2);
+	expect(result[0].length).toBe(2);
+	expect(result[1].length).toBe(2);
+	expect(result[0][0].length).toBe(2);
+	expect(result[0][1].length).toBe(2);
+	expect(result[1][0].length).toBe(3);
+	expect(result[1][1].length).toBe(3);
+
+	expect(result[0][0].indexOf(MESSGAE_TO_SEND1) > -1).toBe(true);
+	expect(result[0][1].indexOf(MESSGAE_TO_SEND1) > -1).toBe(true);
+	expect(result[0][0].indexOf(MESSGAE_TO_SEND2) > -1).toBe(true);
+	expect(result[0][1].indexOf(MESSGAE_TO_SEND2) > -1).toBe(true);
+
+	expect(result[1][0].indexOf(MESSGAE_TO_SEND3) > -1).toBe(true);
+	expect(result[1][1].indexOf(MESSGAE_TO_SEND3) > -1).toBe(true);
+	expect(result[1][0].indexOf(MESSGAE_TO_SEND4) > -1).toBe(true);
+	expect(result[1][1].indexOf(MESSGAE_TO_SEND4) > -1).toBe(true);
+	expect(result[1][0].indexOf(MESSGAE_TO_SEND5) > -1).toBe(true);
+	expect(result[1][1].indexOf(MESSGAE_TO_SEND5) > -1).toBe(true);
+
+	eventBusInstanceA.destory();
+	eventBusInstanceB.destory();
+});
